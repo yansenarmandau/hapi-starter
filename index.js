fffede67c7445f75a974640e179689820ef80593
yansen armandau <yansenarmandau.dev@gmail.com>
2018-01-26 01:08:13 +0700

    add hapi server

---
+++ index.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const Hapi = require('hapi');
+const Glue = require('glue');
+
+const manifest = require('./config/manifest');
+
+const startServer = async () => {
+  try {
+    const server = await Glue.compose(manifest, { relativeTo: __dirname });
+
+    // routes
+    server.route({
+      method: 'GET',
+      path: '/',
+      handler: (request, h) => `i'm Hapi`
+    });
+
+    await server.start();
+    console.log(`Server running at: ${server.info.uri}`);
+
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+};
+
+startServer();
