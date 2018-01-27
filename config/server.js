34da1720ec1be5e38edfb63ae24657f3df59f29e
yansen armandau <yansenarmandau.dev@gmail.com>
2018-01-28 02:29:01 +0700

    add default environtment

--- config/server.js
+++ config/server.js
@@ -2,5 +2,6 @@ require('dotenv').config()
 
 module.exports = {
   host: process.env.NODE_HOST || '0.0.0.0',
-  port: process.env.NODE_PORT || 3030
+  port: process.env.NODE_PORT || 3030,
+  env: process.env.NODE_ENV || 'development'
 }
