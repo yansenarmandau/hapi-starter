976f8ad6a5cf9d27dba695ab37561b0546c9a501
yansen armandau <yansenarmandau.dev@gmail.com>
2018-01-28 00:35:37 +0700

    move routes as plugin

--- index.js
+++ index.js
@@ -8,13 +8,6 @@ const startServer = async () => {
   try {
     const server = await Glue.compose(manifest, { relativeTo: __dirname })
 
-    // routes
-    server.route({
-      method: 'GET',
-      path: '/',
-      handler: (request, h) => `i'm Hapi`
-    })
-
     await server.start()
     console.log(`Server running at: ${server.info.uri}`)
   } catch (err) {
